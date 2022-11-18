17520ac518504f0504f49deae01c6b711b5a4b16
Guiventura6 <guilhermeventurasousa@gmail.com>
2022-11-18 16:14:10 -0300

    Criação da classe codigo no arquivo models.py

--- pastebin_api/codes/apps.py
+++ pastebin_api/codes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CodigosConfig(AppConfig):
+class CodesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'codigos'
+    name = 'codes'
