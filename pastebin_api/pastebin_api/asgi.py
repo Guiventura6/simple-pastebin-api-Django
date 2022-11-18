50ed36452b4e190f4d7b2f77e86002561ca8f319
Guiventura6 <guilhermeventurasousa@gmail.com>
2022-11-18 16:02:39 -0300

    Ajustando projeto

--- pastebin_api/pastebin_api/asgi.py
+++ pastebin_api/pastebin_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for simple_pastebin_api project.
+ASGI config for pastebin_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_pastebin_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pastebin_api.settings')
 
 application = get_asgi_application()
--- pastebin_api/pastebin_api/wsgi.py
+++ pastebin_api/pastebin_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for simple_pastebin_api project.
+WSGI config for pastebin_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_pastebin_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pastebin_api.settings')
 
 application = get_wsgi_application()
